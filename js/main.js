fb3a03f2ae06a931a81a0cf27dab24440406c595
Alexander <xspikyx@gmail.com>
2023-01-20 17:47:25 +0300

    -a.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,40 @@
-import '../scss/main.scss';
+import '../css/main.css';
 
 
-//Hidden menu
-const hamburger = document.querySelector('.hamburger'),
-  hiddenmenu = document.querySelector('.hiddenmenu'),
-  hiddenmenulink = document.querySelector('.hiddenmenu__list'),
-  closeElem = document.querySelector('.hiddenmenu__close'),
-  closeThankyou = document.querySelector('.modal__close'),
-  star = document.querySelectorAll('.hiddenmenu__link');
-const overlay = document.querySelector('.hiddenmenu__overlay');
-const closeClickOverlay = true;
-
-hamburger.addEventListener('click', () => {
-  hiddenmenu.classList.add('active');
-  document.body.style.overflow = "hidden";
-  document.body.style.marginRight = `${scroll}px`;
-});
-
-closeElem.addEventListener('click', () => {
-  hiddenmenu.classList.remove('active');
-  document.body.style.overflow = "";
-  document.body.style.marginRight = `0px`;
-});
-
-hiddenmenulink.addEventListener('click', () => {
-  hiddenmenu.classList.remove('active');
-  document.body.style.overflow = "";
-  document.body.style.marginRight = `0px`;
-});
-//Close menu when click on overlay
-overlay.addEventListener('click', (e) => {
-  if (e.target === overlay && closeClickOverlay) {
+window.onload = () => {
+  //Hidden menu
+  const hamburger = document.querySelector('.hamburger'),
+    hiddenmenu = document.querySelector('.hiddenmenu'),
+    hiddenmenulink = document.querySelector('.hiddenmenu__list'),
+    closeElem = document.querySelector('.hiddenmenu__close'),
+    closeThankyou = document.querySelector('.modal__close'),
+    star = document.querySelectorAll('.hiddenmenu__link');
+  const overlay = document.querySelector('.hiddenmenu__overlay');
+  const closeClickOverlay = true;
+  
+  hamburger.addEventListener('click', () => {
+    hiddenmenu.classList.add('active');
+    document.body.style.overflow = "hidden";
+    document.body.style.marginRight = `${scroll}px`;
+  });
+  
+  closeElem.addEventListener('click', () => {
     hiddenmenu.classList.remove('active');
     document.body.style.overflow = "";
     document.body.style.marginRight = `0px`;
+  });
+  
+  hiddenmenulink.addEventListener('click', () => {
+    hiddenmenu.classList.remove('active');
+    document.body.style.overflow = "";
+    document.body.style.marginRight = `0px`;
+  });
+  //Close menu when click on overlay
+  overlay.addEventListener('click', (e) => {
+    if (e.target === overlay && closeClickOverlay) {
+      hiddenmenu.classList.remove('active');
+      document.body.style.overflow = "";
+      document.body.style.marginRight = `0px`;
+    }
+  });
   }
-});
